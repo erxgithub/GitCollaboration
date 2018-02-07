f07bda8cb30439ad75a245a060a2ec8dbac3a8c5
yongwoohuh <yongwoo.huh@me.com>
2018-02-07 12:36:47 -0500

    Add TableView and master/detail view

---
+++ GitCollaboration/MyTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.m
+//  GitCollaboration
+//
+//  Created by Yongwoo Huh on 2018-02-07.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ GitCollaboration/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  GitCollaboration
+//
+//  Created by Yongwoo Huh on 2018-02-07.
+//  Copyright © 2018 Eric Gregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
